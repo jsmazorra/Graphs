a5c6f2a78d6e834d05060af3fc922858a5c51d4f
jsmazorra <60892622+jsmazorra@users.noreply.github.com>
2020-08-11 01:57:57 -0400

    Modified Stack and Queue

--- projects/graph/util.py
+++ projects/graph/util.py
@@ -2,27 +2,37 @@
 # Note: This Queue class is sub-optimal. Why?
 class Queue():
     def __init__(self):
-        self.queue = []
+        self.list = []
+    def __len__(self):
+        return len(self.list)
+    def __str__(self):
+        return str(self.list)
+    
     def enqueue(self, value):
-        self.queue.append(value)
+        self.list.append(value)
     def dequeue(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.list.pop(0)
         else:
             return None
     def size(self):
-        return len(self.queue)
+        return len(self.list)
 
 class Stack():
     def __init__(self):
-        self.stack = []
+        self.list = []
+    def __len__(self):
+        return len(self.list)
+    def __str__(self):
+        return str(self.list)
+
     def push(self, value):
-        self.stack.append(value)
+        self.list.append(value)
     def pop(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.list.pop()
         else:
             return None
     def size(self):
-        return len(self.stack)
+        return len(self.list)
 
